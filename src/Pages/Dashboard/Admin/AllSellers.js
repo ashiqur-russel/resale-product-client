61004e8eb232d1691118ecb74bb1002f6b215e5a
ashiqurras <ashiqurrussel2@gmail.com>
2022-11-26 00:43:22 +0100

    Display seller information

--- src/Pages/Dashboard/Admin/AllSellers.js
+++ src/Pages/Dashboard/Admin/AllSellers.js
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { data } from "autoprefixer";
 
 const AllSellers = () => {
+  const { data: sellerData = [], refetch } = useQuery({
+    queryKey: ["sellerData"],
+    queryFn: async () => {
+      const res = await fetch(`http://localhost:8000/users`);
+      const data = await res.json();
+      const filter = data.filter((user) => user.role === "seller");
+      console.log(filter);
+      return filter;
+    },
+  });
+
   return (
     <div>
       <div className="bg-white p-8 rounded-md w-full">
@@ -16,7 +29,7 @@ const AllSellers = () => {
                 <thead>
                   <tr>
                     <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                      Photoo
+                      #
                     </th>
                     <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
                       Name
@@ -36,45 +49,50 @@ const AllSellers = () => {
                   </tr>
                 </thead>
                 <tbody>
-                  <tr>
-                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                      <div className="flex items-center">
-                        <div className="flex-shrink-0 w-10 h-10">
-                          <img
-                            className="w-full h-full rounded-full"
-                            src=""
-                            alt="d"
-                          />
-                        </div>
-                        <div className="ml-3">
+                  {sellerData &&
+                    sellerData?.map((seller, i) => (
+                      <tr key={seller._id}>
+                        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                          <div className="flex items-center">
+                            <div className="flex-shrink-0 w-10 h-10">
+                              {i + 1}
+                            </div>
+                            <div className="ml-3">
+                              <p className="text-gray-900 whitespace-no-wrap"></p>
+                            </div>
+                          </div>
+                        </td>
+                        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                           <p className="text-gray-900 whitespace-no-wrap"></p>
-                        </div>
-                      </div>
-                    </td>
-                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                      <p className="text-gray-900 whitespace-no-wrap"></p>
-                    </td>
-                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                      <p className="text-gray-900 whitespace-no-wrap"></p>
-                    </td>
-                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                      <p className="text-gray-900 whitespace-no-wrap"> </p>
-                    </td>
-                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                      <span className="relative inline-block px-3 py-1 font-semibold text-red-900 leading-tight">
-                        <span
-                          aria-hidden
-                          className="absolute inset-0 bg-red-200 opacity-50 rounded-full"
-                        ></span>
-                        <span className="relative">Inactive</span>
-                      </span>
-                    </td>
-                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                      <button className="btb btn-danger bg-red-400 p-2 hover:cursor-pointer">
-                        DELETE
-                      </button>
-                    </td>
-                  </tr>
+                        </td>
+                        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                          <p className="text-gray-900 whitespace-no-wrap">
+                            {" "}
+                            {seller?.email}
+                          </p>
+                        </td>
+                        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                          <p className="text-gray-900 whitespace-no-wrap">
+                            {" "}
+                            {seller?._id}{" "}
+                          </p>
+                        </td>
+                        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                          <span className="relative inline-block px-3 py-1 font-semibold text-red-900 leading-tight">
+                            <span
+                              aria-hidden
+                              className="absolute inset-0 bg-red-200 opacity-50 rounded-full"
+                            ></span>
+                            <span className="relative">{seller?.verified}</span>
+                          </span>
+                        </td>
+                        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                          <button className="btb btn-danger bg-red-400 p-2 hover:cursor-pointer">
+                            DELETE
+                          </button>
+                        </td>
+                      </tr>
+                    ))}
                 </tbody>
               </table>
             </div>
